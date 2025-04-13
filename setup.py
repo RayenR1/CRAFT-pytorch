b82dc99dbd6a3d963af009b96a0e573280630345
RayenR1 <rayen.jlassi@esprit.tn>
2025-04-13 17:18:39 +0100

    bsmelah

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="craft_pytorch_custom",  # Changez le nom pour éviter les conflits
-    version="0.1",
-    packages=["craft_text_detector"],  # Nom du dossier contenant les .py
-    install_requires=[  # Liste des dépendances (ex: torch, opencv)
+    name="craft_text_detector_custom",
+    version="1.0.0",
+    packages=["craft_text_detector"],
+    package_data={"craft_text_detector": ["*.py"]},
+    install_requires=[
         "torch",
-        "opencv-python",
-        "numpy",
+        "opencv-pytho4.5",
+        "numpy"
     ],
 )
